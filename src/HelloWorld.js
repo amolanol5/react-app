10c1a2a37c2595233b693c702feb3e2391823828
andres molano <andresmolano12@gmail.com>
2022-09-26 08:55:00 -0500

    feat: test

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -3,8 +3,11 @@ import React from 'react';
 const HelloWorld = () => {
   
   function sayHello() {
-    const variabletest = process.env.PRUEBA || "varvacia"
-    alert(variabletest);
+    const variabletest1 = process.env.PRUEBA || "varvacia"
+    const variabletest2 = process.env.AWS_APP_ID || "appidvacio"
+    const variabletest3 = process.env.MESSAGE || "messagevacio"
+
+    alert(variabletest1,variabletest2,variabletest3);
   }
   
   return (
